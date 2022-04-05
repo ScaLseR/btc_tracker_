599747e050f3cd1a2189e94f566d404bf5f44fb1
a.skorik <scalserstv@yandex.ru>
2022-04-05 10:40:09 +0300

    added save_to_db, load_from_db

--- sqllite_db.py
+++ sqllite_db.py
@@ -7,10 +7,18 @@ class Sqldata:
         """присваиваем имя нашей БД, настраиваем коннект"""
         self._db_name = db_name
         self._connection = sqlite3.connect(self._db_name)
-        self.cr_table()
+        self._cr_table()
 
-    def cr_table(self):
+    def _cr_table(self):
         """если таблицы нет - создаем, если уже есть то пропускаем"""
         cursor = self._connection.cursor()
         cursor.execute('create table if not exist (date text, price real, unique(date))')
         self._connection.commit()
+
+    def save_to_db(self, data):
+        """сохраняем в базу значения"""
+        pass
+
+    def load_from_db(self, start, end):
+        """получаем из базы значения в интревале start -> end"""
+        pass
