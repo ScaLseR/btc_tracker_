9ac181d7e133d1e901ca7ddec4a7e861dd1ea36b
a.skorik <scalserstv@yandex.ru>
2022-04-06 10:49:01 +0300

    edited load_start_end, _gap_cutting

--- btc_api.py
+++ btc_api.py
@@ -1,16 +1,17 @@
 import json
 import requests
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 class BtcApi:
-    """класс для работы с внешним Api https://old.coindesk.com/coindesk-api"""
+    """класс для работы с Api https://old.coindesk.com/coindesk-api"""
     def __init__(self, n_interval):
         self._url = 'https://api.coindesk.com/v1/bpi/historical/close.json'
         self._n_interval = n_interval
 
     def _request(self, start: datetime, end: datetime) -> dict:
         """get запрос к API"""
+        print('Выполняем запрос к API с ' + str(start) + ' по ' + str(end))
         response = requests.get(self._url, params={'start': str(start), 'end': str(end)})
         resp = json.loads(response.content)
         dict_resp = {}
@@ -18,18 +19,24 @@ class BtcApi:
             dict_resp[key] = value
         return dict_resp
 
+    def _gap_cutting(self, start: datetime, end: datetime) -> dict:
+        """создаем цепочки дат начало и конец = начало + n"""
+        final_dict = {}
+        while start + timedelta(days=self._n_interval) < end:
+            rez = self._request(start, start + timedelta(days=self._n_interval))
+            final_dict.update(rez)
+            start += timedelta(days=self._n_interval)
+            if start > end:
+                start -= timedelta(days=self._n_interval)
+                break
+        rez = self._request(start, end)
+        final_dict.update(rez)
+        return final_dict
 
-    def _gap_cutting(self, start, end):
-        """"""
-        pass
-
-    def load_start_end(self, start: datetime, end: datetime):
+    def load_start_end(self, start: datetime, end: datetime) -> dict:
         """получение данных от API в промежутке start -> end"""
         if (end - start).days < self._n_interval:
-            print('Выполняем запрос к API с ' + str(start) + ' по ' + str(end))
             rez = self._request(start, end)
-            return rez
-
-    def load_start_n_days(self, start, n_days: int):
-        """получение данных от API от начала start и на N дней"""
-        pass
+        else:
+            rez = self._gap_cutting(start, end)
+        return rez
